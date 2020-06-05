ff2043107d59161d9b0de0b1ef190db0f024dcc2
Alesiai <66494457+Alesiai@users.noreply.github.com>
2020-06-05 23:42:01 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const menuBtn = document.querySelector(".m-menu")
+const menu = document.querySelector(".menu-sitebar")
+menuBtn.addEventListener("click", function(){
+    menu.classList.toggle("active")
+})
